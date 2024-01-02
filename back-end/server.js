28e80fd38e0fb1b928d1e5d23dfa8b5b504de9dd
RameshOfficeGitHub <155354198+RameshOfficeGitHub@users.noreply.github.com>
2024-01-02 12:39:46 +0530

    Add files via upload

---
+++ back-end/server.js
@@ -0,0 +1,32 @@
+const exrpress = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = exrpress()
+app.use(exrpress.json());
+app.use(cors());
+// app.use(cors({
+//     origin: [""],
+//     methods: ["POST", 'GET'],
+//     credentials: true
+// }));
+
+// app.get('/', (req, res) => {
+//     res.json("hello")
+// })
+app.post('/user', async (req, res) => {
+
+    try {
+        const { name, age } = req.body;
+        console.log("first")
+        const responce = await axios.post('https://651ff610906e276284c3cac8.mockapi.io/UsersData', { name: name, age: age })
+        res.json({message:"success"})
+    } catch (error) {
+        console.log(error)
+    }
+
+})
+app.listen("8080", "0.0.0.0", () => {
+    console.log(`Server running on port 8080`);
+});
