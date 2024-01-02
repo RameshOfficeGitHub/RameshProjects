b98aa191438af94c360e131faf14307cabf76be7
RameshOfficeGitHub <155354198+RameshOfficeGitHub@users.noreply.github.com>
2024-01-02 12:39:09 +0530

    Add files via upload

---
+++ front-end/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  async function submitFunction() {
+    try {
+      const res = await axios.post('http://localhost:8080/user', { name: "ramesh", age: "24" })
+      console.log("res", res.data.message)
+      alert(res.data.message)
+    } catch (error) {
+      console.log("submitFunction", error)
+    }
+  }
+  return (
+    <div className="App">
+      <button onClick={() => { submitFunction() }}>submit</button>
+    </div>
+  );
+}
+
+export default App;
